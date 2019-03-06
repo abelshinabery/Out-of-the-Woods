717b4950bc79991fa068f6747efc376436377aaf
sfg11 <sfg11@txstate.edu>
2019-03-06 07:51:48 -0600

    adding demo project with file structure

---
+++ demo-project/ui/source/app/App.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style = {styles.welcome}>Helloooo world!</Text>
+        <Button title = "click me"
+                color = "cornflowerblue"
+                onPress={this.pressed} />
+      </View>
+    );
+  }
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
