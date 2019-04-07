8dab6621a10ba052ab2196587a3a5c61ee386fef
serena-marie <serena.gutierrez0@gmail.com>
2019-04-07 10:34:44 -0500

    SOS Basic Outline complete

--- App/foo-app/screens/HomeScreen.js
+++ App/foo-app/screens/HomeScreen.js
@@ -1,102 +1,63 @@
 import React from 'react';
 import {
   Image,
+  Platform,
   ScrollView,
   StyleSheet,
   Text,
   TouchableOpacity,
   View,
-  AppRegistry,
-  Platform,
-  Alert,
-  TouchableNativeFeedback,
-  TouchableWithoutFeedback
+  AppRegistry, // For button, not sure if necessary
 } from 'react-native';
+
 import { WebBrowser } from 'expo';
 
 import { MonoText } from '../components/StyledText';
+
+// Class for SOS Button
+class SosButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { count: 0 };
+  }
+  onPress = () => {
+    this.setState({
+      count: this.state.count + 1,
+    });
+  }
+  
+  render() {
+    return (
+      <View style={styles.sosContainer}>
+        <TouchableOpacity style={styles.circle} onPress={this.onPress}>
+          <Text> SOS </Text>
+        </TouchableOpacity>
+        <View style={[styles.countContainer]}>
+          <Text style={[styles.countText]}>
+            {this.state.count !== 0 ? this.state.count : null}
+          </Text>
+        </View>
+      </View>
+    );
+  }
+} // end SosButton class
 
 export default class HomeScreen extends React.Component {
   static navigationOptions = {
     header: null,
   };
 
-  render() {
-    return (
-      <View style={styles.container}>
-        <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
-          <View style={styles.welcomeContainer}>
-            <Image
-              source={
-                __DEV__
-                  ? require('../assets/images/translogo.gif')
-                  : require('../assets/images/translogo.gif')
-              }
-              style={styles.welcomeImage}
-            />
-          </View>
-
-          <View style={styles.getStartedContainer}>
-            {this._maybeRenderDevelopmentModeWarning()}
-
-            <Text style={styles.myText}>My Text</Text>
-
-            <View style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
-              <MonoText style={styles.codeHighlightText}>screens/HomeScreen.js</MonoText>
-            </View>
-
-            <Text style={styles.getStartedText}>
-              Change this text and your app will automatically reload.
-            </Text>
-          </View>
-
-          <View style={styles.helpContainer}>
-            <TouchableOpacity onPress={this._handleHelpPress} style={styles.helpLink}>
-              <Text style={styles.helpLinkText}>Help, it didn’t automatically reload!</Text>
-            </TouchableOpacity>
-          </View>
-        </ScrollView>
-
-        <View style={styles.tabBarInfoContainer}>
-          <Text style={styles.tabBarInfoText}>This is a tab bar. You can edit it in:</Text>
-
-          <View style={[styles.codeHighlightContainer, styles.navigationFilename]}>
-            <MonoText style={styles.codeHighlightText}>navigation/MainTabNavigator.js</MonoText>
-          </View>
-         </View>
-      </View>
+  render(){
+    return(
+      // Call SosButton class to display
+      <SosButton/>
     );
   }
 
-  _maybeRenderDevelopmentModeWarning() {
-    if (__DEV__) {
-      const learnMoreButton = (
-        <Text onPress={this._handleLearnMorePress} style={styles.helpLinkText}>
-          Learn more
-        </Text>
-      );
-
-      return (
-        <Text style={styles.developmentModeText}>
-          Development mode is enabled, your app will be slower but you can use useful development
-          tools. {learnMoreButton}
-        </Text>
-      );
-    } else {
-      return (
-        <Text style={styles.developmentModeText}>
-
-        Take control of your personal Saftey.
-        <Text onPress={this._handleLearnMorePress} style={styles.helpLinkText}>
-          Learn more
-        </Text>
-        </Text>
-      );
-    }
-  }
-
   _handleLearnMorePress = () => {
-    WebBrowser.openBrowserAsync('https://docs.expo.io/versions/latest/guides/development-mode');
+    WebBrowser.openBrowserAsync(
+      'https://docs.expo.io/versions/latest/guides/development-mode'
+    );
   };
 
   _handleHelpPress = () => {
@@ -104,110 +65,31 @@ export default class HomeScreen extends React.Component {
       'https://docs.expo.io/versions/latest/guides/up-and-running.html#can-t-see-your-changes'
     );
   };
-}
+} //end HomeScreen class
 
+// Style sheets
 const styles = StyleSheet.create({
-  container: {
-  paddingTop: 20,
-  // alignItems: 'center'
-},
-button: {
-  marginBottom: 30,
-  width: 260,
-  alignItems: 'center',
-  backgroundColor: '#2196F3'
-
-
-},
-buttonText: {
-  padding: 20,
-  fontWeight: '700',
-  color: 'white',
-
-},
-  developmentModeText: {
-    marginBottom: 20,
-    color: 'rgba(0,0,0,0.4)',
-    fontSize: 14,
-    lineHeight: 19,
-    textAlign: 'center',
+  sosContainer: {
+    flex: 1, // makes the component flexible and it will be sized proportional to its flex value.
+    justifyContent: 'center', //centers circle vertically
+    alignItems: 'center', //centers circle horizontally
   },
-  contentContainer: {
-    paddingTop: 30,
-  },
-  welcomeContainer: {
-    alignItems: 'marginLeft',
-    marginTop: 10,
-    marginBottom: 20,
-  },
-  welcomeImage: {
-    width: 120,
-    height: 100,
-    resizeMode: 'contain',
-    marginTop: -10,
-    marginLeft: 0,
-  },
-  getStartedContainer: {
+  countContainer: {
     alignItems: 'center',
-    marginHorizontal: 50,
+    padding: 10,
   },
-  homeScreenFilename: {
-    marginVertical: 0,
+  countText: {
+    color: '#FF00FF',
   },
-  codeHighlightText: {
-    color: 'rgba(96,100,109, 0.8)',
+  circle: {
+    alignItems: 'center', //align SOS text horizontallt
+    justifyContent: 'center', //align SOS text vertically
+    width: 200, // if adj this, need to adj height & radius with same values
+    height: 200,
+    borderRadius: 200 / 2,
+    backgroundColor: '#DDDDDD',
+    padding: 10,
   },
-  codeHighlightContainer: {
-    backgroundColor: 'rgba(0,0,0,0.05)',
-    borderRadius: 3,
-    paddingHorizontal: 4,
-  },
-  myText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    lineHeight: 24,
-    textAlign: 'left',
-    paddingBottom: 50,
-    paddingRight: 50,
-  },
-  tabBarInfoContainer: {
-    // position: 'relative',
-    // bottom: 30,
-    // left: 0,
-    // right: 0,
-    // ...Platform.select({
-    //   ios: {
-    //     shadowColor: 'black',
-    //     shadowOffset: { height: -3 },
-    //     shadowOpacity: 0.1,
-    //     shadowRadius: 3,
-    //   },
-    //   android: {
-    //     elevation: 20,
-    //   },
-    // }),
-    // alignItems: 'center',
-    // backgroundColor: '#2d461f',
-    // paddingVertical: 20,
-  },
-  tabBarInfoText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    textAlign: 'center',
-  },
-  navigationFilename: {
-    marginTop: 5,
-  },
-  helpContainer: {
-    marginTop: 15,
-    alignItems: 'center',
-  },
-  helpLink: {
-    paddingVertical: 15,
-  },
-  helpLinkText: {
-    fontSize: 14,
-    color: '#2e78b7',
-  },
-
 });
+
+//AppRegistry.registerComponent('App', () => HomeScreen)
